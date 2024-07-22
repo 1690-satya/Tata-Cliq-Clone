0a3b003d965105b1e9453cd0275a2105d0818020
phytoplankton <134634257+1690-satya@users.noreply.github.com>
2024-07-22 10:55:22 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './styles.css'; // Import the CSS file
+import { CartProvider } from './CartContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ChakraProvider>
     <BrowserRouter>
-      <App />
+      <CartProvider>
+        <App />
+      </CartProvider>
     </BrowserRouter>
   </ChakraProvider>
 );
