7a07313f0b4e129ebe67524a628ff3ea708bc10d
phytoplankton <134634257+1690-satya@users.noreply.github.com>
2024-07-22 10:36:20 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,16 @@
-
-// import { Routes, Route } from 'react-router-dom';
-// import HomePage from './pages/HomePage';
-// import LoginPage from './components/LoginPage'
-// import Signup from './pages/Signup';
-// import ProductPage from './pages/ProductPage';
-//import Cart from './pages/Cart';
 import Navbar from './components/Navbar';
-// import Carousel from './components/Carousel'
-// import BlockbusterDeals from './pages/BlockbusterDeals';
-// import JuniorsFavorites from './pages/JuniorFavorites';
-// import TickTalk from './pages/TickTalk';
+
 import AllRoutes from './AllRoutes';
+import Footer from './components/Footer';
+import Socials from './pages/Socials';
 
 const App = () => (
   <>
     
       <Navbar />
       <AllRoutes />
-    
+      <Socials />
+      <Footer />
   </>
 
 );
-
-export default App;
-
