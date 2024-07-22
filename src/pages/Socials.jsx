7c42bb2403071b3732a1f5442af60fb37e16d749
phytoplankton <134634257+1690-satya@users.noreply.github.com>
2024-07-22 10:33:06 +0530

    Create Socials.jsx

---
+++ src/pages/Socials.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import './Socials.css'
+function Socials(){
+    return(
+        <>
+        <div id="mainDiv">
+            <div class="cdiv">
+                <p><b>Download App</b></p>
+                <a href="https://play.google.com/store/apps/details?id=com.tul.tatacliq&pli=1"><img class="icon" src="https://www.clipartmax.com/png/middle/61-616333_android-black-logo-hd-wallpapers-android-black-logo-android-logo-black-png.png"/></a>
+                <a href="https://apps.apple.com/us/app/tata-cliq-online-shopping-app/id1101619385?ls=1"><img class="icon" src="https://static.vecteezy.com/system/resources/previews/002/520/838/original/apple-logo-black-isolated-on-transparent-background-free-vector.jpg" /></a>
+            </div>
+            <div class="cdiv">
+                <a href="https://www.facebook.com/TataCLiQ/">
+                    <img class="icon" src="https://cdn3.iconfinder.com/data/icons/social-media-2527/24/glyph_facebook_facebook_logo_logo_logotype_letter_f_social_media_social_media-512.png"/>
+                </a>
+                <a href="https://x.com/tatacliq">
+                    <img class="icon" src="https://img.freepik.com/free-vector/new-2023-twitter-logo-x-icon-design_1017-45418.jpg?t=st=1721588444~exp=1721592044~hmac=550083000742c2d0abbd84beacdf3456ec4a9a5d042c35be8c77fdf95a26881a&w=740"/>
+                </a>
+                <a href="https://www.instagram.com/tatacliq/">
+                    <img class="icon" src="https://i.pinimg.com/736x/41/e4/57/41e457e8599833299d563976b7d3c59a.jpg"/>
+                </a>    
+                <a href="https://www.youtube.com/channel/UCUwkaWqIcl9dYQccKkM0VRA">
+                    <img class="icon" src="https://i.pinimg.com/564x/e8/c9/82/e8c9828387de2a83e5469a95bc6d8574.jpg"/>
+                </a>
+                <a href="https://www.linkedin.com/authwall?trk=bf&trkInfo=AQGuF37dd95cRwAAAZDWrvaIHGbJgNeK7ScXdQt0X2Awe5zXTqbVkE68E_hay3vHKa0MKQ0kRkY1-5Zss56H-8cSyqHU49NHthf4QgMYwqHoI1IbpWl0Yr1vZ3JnJEF1n0E8ey0=&original_referer=&sessionRedirect=https%3A%2F%2Fin.linkedin.com%2Fcompany%2Ftatacliq">
+                    <img class="icon" src="https://cdn-icons-png.flaticon.com/512/61/61109.png"/>
+                </a>
+            </div>
+            <div id="div3">© 2024 Tata CLiQ | All rights reserved</div>
+        </div>
+        <br></br>
+        </>
+    );
+}
+export default Socials;
