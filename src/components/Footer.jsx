b44806a999bfa16708cbb5eef37158408b2c25d9
phytoplankton <134634257+1690-satya@users.noreply.github.com>
2024-07-22 10:30:42 +0530

    Update Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+function Footer(){
+  return(
+      <>
+          <h1><b>TATA CLiQ: Shop Online with India's most trusted destination</b></h1>
+          <br></br>
+          <p>Genuine products from all the top brands get delivered right to your doorstep. Our sleek, immersive design allows you to easily navigate between categories and brand stores so that you can find a wide selection of womenswear, menswear, kidswear, footwear, watches, accessories, footwear, watches and accessories online. You can also check our great offers and get the best prices on various products across lifestyle, fashion, and more.</p>
+          <br></br>
+          <h1><b>Online Shopping: Fast & convenient with the click of a button</b></h1>
+          <br></br>
+          <p>The upside of online shopping at TATA CLiQ online store, is that you'll save on time and most importantly money with TataCliq offers. It's as simple as comparing products and prices online before making the right buy. What's more, you also have the option to pay for your favourite brands and products using our easy EMI options. Of course, you can buy and try - in the convenience of your home. Returns are easy too: We'll pick up your returns for free or you can also drop off the goods at the nearest brand store.</p>
+          <br></br>
+          <h1><b>Tata CLiQ Shopping App: just a few clicks on Android & iOS</b></h1>
+          <br></br>
+          <p>Download the Android app from the <a href="https://play.google.com/store/apps/details?id=com.tul.tatacliq"><u>Play Store</u></a> or the iOS app from <a href="https://apps.apple.com/us/app/tata-cliq-online-shopping-app/id1101619385?ls=1"><u>Apple App Store</u></a> and get set to enjoy a range of benefits. Apart from the best deals, amazing offers and the latest styles online, the app also gives you the flexibility to shop at your convenience. Use the easy share options to share your shopping with your friends and family to ensure you're buying something perfect. With constant updates and a host of new features being introduced constantly, enjoy a shopping experience that you'll love.</p>
+          <br></br>
+          <h1><b>TATA CLiQ: The most genuine place for Fashion and Lifestyle</b></h1>
+          <br></br>
+          <p>With an exclusive Brand Store for Westside Online we have most of your trendy shopping needs taken care of. Make TATA CLiQ your online shopping destination and get the best deals on your favourite brands, with 100% genuine products. Be it jewellery or makeup, you can count on TATA CLiQ for receiving only the most authentic products.</p>
+          <br></br>
+      </> 
+  );
+}
+export default Footer;
